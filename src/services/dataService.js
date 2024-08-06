d3e37e40dbac2774d9545813109debb27fcd29d9
pedzero <pedro9669silva@gmail.com>
2024-08-06 20:30:20 -0300

    refactor: remove ';'

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -7,20 +7,20 @@ const store = async (buffer) => {
         const [institute] = await Institute.findOrCreate({
             where: { name: registry.institute },
             defaults: { name: registry.institute }
-        });
+        })
 
         const [room] = await Room.findOrCreate({
             where: { name: registry.room, institute_id: institute.id },
             defaults: { name: registry.room, institute_id: institute.id }
-        });
+        })
 
         await Parameter.create({
             name: registry.parameter,
             value: registry.value,
             room_id: room.id,
             timestamp: registry.timestamp
-        });
+        })
     }
-};
+}
 
-export default { store };
+export default { store }

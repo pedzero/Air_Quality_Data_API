639ab76b487e77885738659a6da7dbeff514da66
pedzero <pedro9669silva@gmail.com>
2024-08-06 20:38:42 -0300

    feat: add dataController with error handling

---
+++ src/controllers/dataController.js
@@ -0,0 +1,16 @@
+import bufferService from '../services/dataBufferService.js'
+
+const controller = {}
+
+controller.create = (request, response) => {
+    const data = request.body
+
+    try {
+        bufferService.addDataToBuffer(data)
+        response.sendStatus(200)
+    } catch (error) {
+        response.status(400).json({ error: "Bad request", message: error.message })
+    }
+}
+
+export default controller
--- src/routes/dataRouter.js
+++ src/routes/dataRouter.js
@@ -1,22 +1,9 @@
 import express from 'express'
 
-import buffer from '../services/dataBufferService.js'
+import dataController from '../controllers/dataController.js'
 
 const router = express.Router()
 
-router.post('/api/data', (request, response) => {
-    const data = request.body
-    let clientIp = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
-
-    if (clientIp.startsWith('::ffff:')) {
-        clientIp = clientIp.replace('::ffff:', '');
-    }
-
-    console.log(`Adding new content in buffer from ${clientIp}.`)
-    
-    buffer.addDataToBuffer(data)
-
-    response.sendStatus(200)
-})
+router.post('/api/data', dataController.create)
 
 export default router

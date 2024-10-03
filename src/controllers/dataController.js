e0c9bad5fda6dca7c6d0238ae07d5b8cf60fd128
pedzero <pedro9669silva@gmail.com>
2024-10-03 20:21:53 -0300

    feat: add communication check case when body is empty

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -5,8 +5,12 @@ const controller = {}
 controller.create = (request, response) => {
     const data = request.body
     try {
-        bufferService.addDataToBuffer(data)
-        response.sendStatus(200)
+        if (Object.keys(data).length === 0) {
+            response.sendStatus(200)
+        } else {
+            bufferService.addDataToBuffer(data)
+            response.sendStatus(200)
+        }
     } catch (error) {
         response.status(400).json({ error: "Bad request", message: error.message })
     }

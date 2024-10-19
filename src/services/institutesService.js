cab7557ad9c79202ce4f129cc640040e2dd5497e
pedzero <pedro9669silva@gmail.com>
2024-10-19 17:27:38 -0300

    fix: correct case when no city is provided

--- src/services/institutesService.js
+++ src/services/institutesService.js
@@ -1,17 +1,19 @@
 import CityRepository from '../repositories/CityRepository.js'
 import InstituteRepository from '../repositories/InstituteRepository.js'
-import { NotFoundError } from '../errors/CustomErrors.js'
+import { NotFoundError, ValidationError } from '../errors/CustomErrors.js'
 
 const retrieve = async (filters) => {
     const { city, name } = filters
-    console.log(filters)
 
     if (!city?.trim() && !name?.trim()) {
-        console.log("All")
         return await InstituteRepository.findAll()
     }
 
-    let city_query = await CityRepository.findOneByName(city.trim())
+    if (!city?.trim()) {
+        throw new ValidationError("Unable to produce a search with the given parameters.")
+    }
+
+    const city_query = await CityRepository.findOneByName(city.trim())
     if (!city_query) {
         throw new NotFoundError(`City '${city}' not found.`)
     }

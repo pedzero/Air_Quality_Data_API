e08829e7bcb0212c772852317aceec3a19fb0912
pedzero <pedro9669silva@gmail.com>
2024-11-30 20:51:23 -0300

    feat: add database port option from environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,7 @@ dotenv.config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
 })
